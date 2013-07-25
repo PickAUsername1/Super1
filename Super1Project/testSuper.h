ac52fa098cf14cd7f4ba8a7f6d6636f771089817
Mariam Hakobyan <hakobyanmariam@attask.com>
2013-07-25 16:41:00 +0400

    - Added test files

---
+++ Super1Project/testSuper.h
@@ -0,0 +1,13 @@
+//
+//  testSuper.h
+//  Super1Project
+//
+//  Created by Mariam Hakobyan on 7/25/13.
+//  Copyright (c) 2013 Mariam Hakobyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testSuper : NSObject
+
+@end
---
+++ Super1Project/testSuper.m
@@ -0,0 +1,13 @@
+//
+//  testSuper.m
+//  Super1Project
+//
+//  Created by Mariam Hakobyan on 7/25/13.
+//  Copyright (c) 2013 Mariam Hakobyan. All rights reserved.
+//
+
+#import "testSuper.h"
+
+@implementation testSuper
+
+@end
